e3930e87e15a2b5a75ec25fb1e8e14007cd3da36
Christopher Cliff <chris@percolate.com>
2015-01-17 12:39:55 -0600

    Refactored into standalone function.

--- test/index.js
+++ test/index.js
@@ -1,4 +1,6 @@
+var _ = require('lodash')
 var assertDir = require('assert-dir-equal')
+var BPromise = require('bluebird')
 var fs = require('fs')
 var watch = require('../')
 var Metalsmith = require('metalsmith')
@@ -6,29 +8,49 @@ var mkdirp = require('mkdirp')
 var path = require('path')
 var rimraf = require('rimraf')
 
-describe('metalsmith-simplewatch', function(){
+describe('metalsmith-simplewatch', function () {
+
+    this.timeout(6e3)
 
     var srcPath = path.resolve(__dirname, './fixtures/basic/src/')
     var buildPath = path.resolve(__dirname, './fixtures/basic/build/')
     var expectedPath = path.resolve(__dirname, './fixtures/basic/expected/')
 
-    beforeEach(function(){
+    beforeEach(function () {
         rimraf.sync(srcPath)
         mkdirp.sync(srcPath)
-        fs.writeFileSync(path.resolve(srcPath, 'a.js'))
-        console.log('setup finished')
+        fs.writeFileSync(path.resolve(srcPath, 'a.js'), '')
     })
 
-    it('should rebuild when a file is changed', function(done){
-        (new Metalsmith('test/fixtures/basic'))
-            .use(watch())
-            .build(function(err){
-                if (err) return done(err)
-                assertDir(srcPath, buildPath)
-                fs.renameSync(path.resolve(srcPath, './a.js'), path.resolve(srcPath, './b.js'))
-                assertDir(expectedPath, buildPath)
-                return done(null)
+    it('should rebuild when a file is changed', function (done) {
+        done = _.once(done)
+        var timesBuilt = 0
+        watch({
+            buildFn: build,
+            buildPath: buildPath,
+            srcPath: srcPath,
         })
+        BPromise
+            .delay(2e3)
+            .then(function () {
+                timesBuilt.should.equal(1)
+                assertDir(srcPath, buildPath)
+            })
+            .catch(done)
+            .then(function () {
+                fs.renameSync(path.resolve(srcPath, './a.js'), path.resolve(srcPath, './b.js'))
+            })
+            .delay(2e3)
+            .then(function () {
+                timesBuilt.should.equal(2)
+                assertDir(expectedPath, buildPath)
+            })
+            .catch(done)
+            .then(done)
+        function build() {
+            timesBuilt++
+            (new Metalsmith('test/fixtures/basic')).build()
+        }
     })
 
 })
